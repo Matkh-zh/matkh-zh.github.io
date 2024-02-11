099313e7376b59f9752f27cad0029fcdd8285e1a
Matkh-zh <130236241+Matkh-zh@users.noreply.github.com>
2024-02-11 22:33:15 +0800

    Add files via upload

---
+++ js/meiju.js
@@ -0,0 +1,17 @@
+// 定义一个数组存储每日诗词的列表
+var poems = [
+    "未来可期 人生值得",
+    "前路浩浩荡荡 万物皆可期待",
+    "不要等待 因为你不知道要等待多长时间",
+];
+
+// 获取当前日期
+var currentDate = new Date();
+
+// 使用日期的天数作为索引值从诗词数组中获取每日诗词
+var poemIndex = currentDate.getDate() % poems.length;
+var dailyPoem = poems[poemIndex];
+
+// 将每日诗词显示在 h1 元素中
+var poemTitleElement = document.getElementById("poem-title");
+poemTitleElement.textContent = dailyPoem;
